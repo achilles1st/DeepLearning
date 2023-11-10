4d609fd954c895ed97e4d9df228b1d2801c944dd
Pand0riaGit <niklas.pratscher@student.tugraz.at>
2023-11-10 16:07:07 +0100

    fixed error print, fixed early stopping, added iterations and safe as png

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense, Input, Dropout, Activation
 from keras.callbacks import EarlyStopping
+
 # ------------------------------------------------------ a -------------------------------------------------------------
 
 # loading data
@@ -33,16 +34,15 @@ x_test = scaler.transform(x_test)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
 # print main info about the investigate feature distributions
 df_info = df.describe()
-#print(df_info)
+# print(df_info)
 
 # Visualize feature distributions
 plt.figure(figsize=(12, 8))
 for i, column in enumerate(df.columns):
-    plt.subplot(3, 3, i+1)
+    plt.subplot(3, 3, i + 1)
     df[column].hist(bins=40)
     plt.title(column)
 plt.show()
-
 
 
 # ------------------------------------------------------ b -------------------------------------------------------------
@@ -65,21 +65,20 @@ def create_NNM(hidden_units, hidden_layers):
     model.add(Dense(1, activation='linear'))
     # Compiling the model
     model.compile(loss='mean_squared_error', optimizer="adam", metrics=['mse'])
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', restore_best_weights=True)
-    return model, model_name
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
+    return model, model_name, early_stopping
 
 
 # Define a function to train the model and return training and validation errors
-def train_evaluate_model(model, x_train, y_train, x_val, y_val, batch_size, epochs, model_name, x_test, y_test):
+def train_evaluate_model(model, x_train, y_train, x_val, y_val, batch_size, epochs, model_name, x_test, y_test,
+                         early_stopping):
     # Configure the model training procedure
     history = model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=batch_size, epochs=epochs,
-                        verbose=0)
+                        verbose=0, callbacks=[early_stopping])
 
     test_loss, test_accuracy = model.evaluate(x_test, y_test, batch_size=batch_size)
 
-
-
-    # plot results
+    # # # plot results
     plt.figure()
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -87,15 +86,19 @@ def train_evaluate_model(model, x_train, y_train, x_val, y_val, batch_size, epoc
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='best')
-    plt.show()
+    # plt.show()
 
-    return test_loss, test_accuracy
+    plt.savefig('figures/{}.png'.format(model_name) + f"_batch_{batch_size}")
+
+    val_error = history.history['val_loss']
+    return test_loss, test_accuracy, val_error[-1]
+
 
 # Define hyperparameters and architectures to test
-hidden_units_list = [64, 128, 256]
+hidden_units_list = [4, 8, 16, 32, 64, 128, 256]
 hidden_layers_list = [1, 2, 3]
-batch_sizes = [16, 32, 64]
-epochs = 70
+batch_sizes = [4, 8, 16, 32,64,128,256]
+epochs = 300
 
 # Create a table to store results
 results = []
@@ -103,18 +106,18 @@ for batch_size in batch_sizes:
     # Loop through different architectures and evaluate
     for hidden_units in hidden_units_list:
         for hidden_layers in hidden_layers_list:
-            model, model_name = create_NNM(hidden_units, hidden_layers)
-            test_loss, test_accuracy = train_evaluate_model(model, x_train, y_train, x_val, y_val, batch_size, epochs,
-                                                          model_name, x_test, y_test)
+            model, model_name, early_stopping = create_NNM(hidden_units, hidden_layers)
+            test_loss, test_accuracy, val_error = train_evaluate_model(model, x_train, y_train, x_val, y_val,
+                                                                       batch_size, epochs, model_name, x_test, y_test,
+                                                                       early_stopping)
 
-            results.append([hidden_units, hidden_layers, test_loss, batch_size])
+            results.append([hidden_units, hidden_layers, test_loss, batch_size, val_error])
+            print(hidden_layers)
 
 # Print the results in a table
-print("Architecture | Hidden Units | Hidden Layers | Train Error | batch size")
+print(" Hidden Units | Hidden Layers | batch size | Train Error | Val Error")
 for result in results:
-    print(
-        f"{result[0]}-{result[1]}    | {result[0]}     | {result[1]}     | {result[2]:.4f}      | {result[3]:.4f}")
-
+    print("{}|{}|{}|{}|{}".format(result[0], result[1], result[3], result[2], result[4]))
 
 # ------------------------------------------------------ c -------------------------------------------------------------
 hidden_unit = 128
@@ -122,10 +125,5 @@ hidden_layer = 2
 learning_rates = [0.01]
 
 # optimizers
-#sgd = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9, nesterov=False)
-#adam = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=False)
-
-
-
-
-
+# sgd = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9, nesterov=False)
+# adam = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=False)
