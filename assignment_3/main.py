9edb764c7d7246411132d2eb224b00407a5f6781
Pand0riaGit <niklas.pratscher@student.tugraz.at>
2023-11-28 13:47:16 +0100

    First model

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 from sklearn.model_selection import train_test_split
-
-
-
-
+from keras.optimizers import Adam
+from keras.optimizers import schedules
+from keras.models import Sequential
+from tensorflow.keras import layers, models
 
 
 class CNN:
@@ -16,11 +16,11 @@ class CNN:
         self.x_perturb, self.y_perturb = dict["x_perturb"], dict["y_perturb"]
         self.x_perturb = np.mean(self.x_perturb, axis=3)
 
+        self.x_train, self.x_val, self.y_train, self.y_val = train_test_split(
+            self.x_train, self.y_train, test_size=0.25, random_state=42, stratify=self.y_train
+        )
 
     def execute_a(self):
-        self.x_train, self.x_val, self.y_train, self.y_val = train_test_split(
-            self.x_train, self.y_train, test_size=0.25, random_state=42, stratify = self.y_train
-        )
 
         print(self.x_train.shape)
         print(self.y_train.shape)
@@ -41,11 +41,55 @@ class CNN:
             plt.title(self.y_train[unique[index]])
         plt.show()
 
+    def execute_b(self):
 
+        # hidden_units_list = [128, 64, 32, 8]
+        # hidden_layers_list = [3, 2, 1]
+        # batch_sizes = [256, 128, 64, 16]
+        # epochs = 300
+        # learning_rates = [0.1, 0.01, 0.001, 0.0001]
+        # scheduled = [True, False]
+
+        hidden_units_list = [64]
+        hidden_layers_list = [2]
+        batch_sizes = [64]
+        epochs = 300
+        learning_rates = [0.01]
+        scheduled = [True]
+
+        model, modelname = self.create_CNN_classification(hidden_units_list, hidden_layers_list, learning_rates, scheduled)
+
+    def create_CNN_classification(self, hidden_units, hidden_layers, learning_rate, scheduled):
+        model_name = "hl_{}_hu_{}".format(hidden_layers, hidden_units)
+        # Configure the model layers
+        model = Sequential()
+        # Input layer
+        if scheduled:
+            learning_rate = schedules.ExponentialDecay(learning_rate, decay_steps=1000, decay_rate=0.9, staircase=True)
+
+        for _ in range(len(hidden_layers)):
+            model.add(layers.Conv2D(hidden_units, (3, 3), activation='relu', input_shape=(32, 32, 1)))
+            model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Flatten())
+
+        model.add(layers.Dense(512, activation='relu'))
+        model.add(layers.Dense(20, activation='sigmoid'))  # For binary classification
+
+        model.compile(optimizer=Adam(learning_rate=learning_rate),
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.summary()
+
+        return model, model_name
 
 
 if __name__ == '__main__':
     dict = pickle.load(open("C:/Users/nikla/Uni/Mit_Stef/DeepLearning/cifar20_perturb_test.pkl", "rb"))
     train_data, test_data = tfd.cifar100.load_data(label_mode="coarse")
     model_instance = CNN(train_data, test_data, dict)
-    model_instance.execute_a()
+
+    # model_instance.execute_a()
+
+    model_instance.execute_b()
